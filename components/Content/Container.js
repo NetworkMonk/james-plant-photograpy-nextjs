4120ed144a5a918433cb549edd20226595f2c871
NetworkMonk <jamesplant@gmail.com>
2023-01-24 22:28:02 +0000

    Align container to the left in mobile

--- components/Content/Container.js
+++ components/Content/Container.js
@@ -2,8 +2,8 @@ export default function Container(props) {
   const { children } = props;
 
   return (
-    <div className="text-white p-10 mt-auto align-top absolute bottom-0 right-0 h-[calc(100vh-8rem)] lg:h-[calc(100vh-13rem)] max-h-[calc(100vh-8rem)] max-lg:h-[calc(100vh-13rem)] overflow-auto lg:text-right scroll-smooth ">
+    <div className="text-white p-10 mt-auto align-top absolute bottom-0 right-0 h-[calc(100vh-8rem)] lg:h-[calc(100vh-13rem)] max-h-[calc(100vh-8rem)] max-lg:h-[calc(100vh-13rem)] overflow-auto lg:text-right scroll-smooth left-0 lg:left-auto">
       {children}
     </div>
-  )
+  );
 }
